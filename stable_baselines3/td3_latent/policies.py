77793947335c6b14747c2c5179a7c05c93289ffd
Nicholas Corrado <ncorrado@wisc.edu>
2022-01-28 00:40:16 -0600

    TD3 Latent supports OH noise (#3)
    Co-authored-by: Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>

--- stable_baselines3/td3_latent/policies.py
+++ stable_baselines3/td3_latent/policies.py
@@ -95,7 +95,9 @@ class Actor(BasePolicy):
         features = self.extract_features(obs)
         mu = self.mu(features)
         if not deterministic:
-            mu += th.from_numpy(self.action_noise()[:self.latent_dim])
+            noise = self.action_noise()
+            print(noise)
+            mu += th.from_numpy(noise[:self.latent_dim])
         mu = self.decoder(mu)
         mu = th.tanh(mu)
         return mu
